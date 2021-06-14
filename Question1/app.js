ab03371d0aa1aefee41fbd88e824d52f96312bf7
Adhish Bahl <adhishbahl0@gmail.com>
2021-06-15 00:30:46 +0530

    Question1-Done

--- Question1/app.js
+++ Question1/app.js
@@ -1,7 +1,7 @@
 var numOne = document.querySelector("#firstnum");
 var numTwo = document.querySelector("#secondnum");
 var add = document.querySelector("#add");
-var subtract = document.querySelector("#subract");
+var subtract = document.querySelector("#subtract");
 var multiply = document.querySelector("#multiply");
 var divide = document.querySelector("#divide");
 var output = document.querySelector("#output");
@@ -9,18 +9,23 @@ var output = document.querySelector("#output");
 function sum() {
     var first = numOne.value;
     var two = numTwo.value;
-    console.log(first, two);
-    var result = first+two;
-    output.innerText = result;
+    output.innerText = parseInt(first) + parseInt(two);
 }
 function minus() {
-    output.innerText = first - two;
+    var first = numOne.value;
+    var two = numTwo.value;
+    console.log(first);
+    output.innerText = parseInt(first) - parseInt(two);
 }
 function into() {
-    output.innerText = first * two;
+    var first = numOne.value;
+    var two = numTwo.value;
+    output.innerText = parseInt(first) * parseInt(two);
 }
 function upon() {
-    output.innerText = first / two;
+    var first = numOne.value;
+    var two = numTwo.value;
+    output.innerText = parseInt(first) / parseInt(two);
 }
 
 add.addEventListener("click", sum);
