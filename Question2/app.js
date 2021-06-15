a0fddcbeb4fdf632c62fbc004fbaf6bace513cb4
Adhish Bahl <adhishbahl0@gmail.com>
2021-06-15 23:43:59 +0530

    Code cleaned

--- Question2/app.js
+++ Question2/app.js
@@ -8,15 +8,7 @@ var x = 0;
 function bigger () {
     let inVal = input.value;
     output.innerHTML = inVal;
-
-    // var fontSizee = parseInt($("#output").css("font-size"));
-    // output.style.backgroundColor = "red";
-    // fontSizee = 1 + "rem + " + (x*2) + "px";
-    // console.log(fontSizee);
-    // x = x + 1;
-
     input.style.fontSize = "30px";
-    // input.style.backgroundColor = "red";
 }
 
 increase.addEventListener("click", bigger);
