70e66a5f62696532f31685abc280e75b14c3d970
Adhish Bahl <adhishbahl0@gmail.com>
2021-06-17 00:48:24 +0530

    Q3 Done with some restristions

--- Question3/app.js
+++ Question3/app.js
@@ -4,19 +4,17 @@ const H2 = document.querySelector("#h2");
 const H3 = document.querySelector("#h3");
 const output = document.querySelector("#output");
 
-
-
 H1.addEventListener("click", ()=> {
-    console.log(input.val);
+    output.remove;
     output.outerHTML = '<h1 class="output" id="output">' + input.value + '</h1>';
 });
 
 H2.addEventListener("click", ()=> {
-    console.log(input.val);
+    output.remove;
     output.outerHTML = '<h2 class="output" id="output">' + input.value + '</h2>';
 });
 
 H3.addEventListener("click", ()=> {
-    console.log(input.val);
+    output.remove;
     output.outerHTML = '<h3 class="output" id="output">' + input.value + '</h3>';
 });
