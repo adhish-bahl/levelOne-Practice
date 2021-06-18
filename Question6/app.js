994d41a97d18a792443cef9f16fa426afafc7379
Adhish Bahl <adhishbahl0@gmail.com>
2021-06-18 23:39:27 +0530

    added comments difference between display and visiblity

--- Question6/app.js
+++ Question6/app.js
@@ -2,6 +2,11 @@ const heading = document.querySelector("#heading");
 const button = document.querySelector(".button button");
 
 button.addEventListener("click", ()=> {
-    heading.style.display = "none";
+    heading.style.visibility = "hidden";
+    // visiblity reserves the space for the element /
+    
+    // heading.style.display = "none";
+    // display does not reserves the space for the element and while shifts all the other elements up!
+
     console.log("click");
 });
