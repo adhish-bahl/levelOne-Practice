5203a5dfa7c16f349cf3917346e25dc541a22c17
Adhish Bahl <adhishbahl0@gmail.com>
2021-06-19 23:31:21 +0530

    Q7 trying getting response from api

--- Question7/app.js
+++ Question7/app.js
@@ -1,10 +1,21 @@
 const button = document.querySelector("#button");
 const input = document.querySelector("#input");
-const output = document.querySelector("#output");
+const output = document.querySelector("#outputPara");
 const api = "https://api.funtranslations.com/translate/minion.json?text=";
 
 button.addEventListener("click", ()=> {
     var inputValue = input.value;
-    api = api.concat(inputValue);
-    console.log(api);
+    // const finalapi = api.concat(inputValue);
+    const finalapi = api + inputValue;
+
+    fetch(finalapi)
+    .then(response => response.json())
+    .then(json => {
+        var outputtext = json.contents.translated;
+        output.innerText = outputtext;
+    })
+    .catch(()=> {
+        alert("Error occured");
+    })
+    // console.log(finalapi);
 });
