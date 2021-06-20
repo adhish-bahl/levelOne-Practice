4783af3f7dc09b5af68fd57e6f91b07b563e7784
Adhish Bahl <adhishbahl0@gmail.com>
2021-06-20 22:22:51 +0530

    JS done

--- Question8/app.js
+++ Question8/app.js
@@ -1,14 +1,23 @@
-const input = document.querySelector("#id");
+const input = document.querySelector("#input");
 const button = document.querySelector("#button");
-const output = document.querySelector("#outputPara");
+const output = document.querySelector(".outputPara");
 const api = "https://api.funtranslations.com/translate/minion.json?text="
 
 button.addEventListener("click", ()=> {
     var inputValue = input.value;
     const finalcall = api + inputValue;
+
     fetch(finalcall)
     .then(response => response.json)
     .then(json => {
-        output.innerText = json.error.message;
+        var outputtext = json.contents.translated;
+        output.innerText = outputtext;
+    })
+    .catch(json=> {
+        errorMessage = json.error.message;
+        console.log(errorMessage);
+        // output.innerText = response.error.message;
+        // output.innerText = json.error.code;
+        // alert("Error: error occured");
     })
 })
