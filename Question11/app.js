56892458e31e1940250f87db7f71dbecec7cfd26
Adhish Bahl <adhishbahl0@gmail.com>
2021-06-22 00:27:25 +0530

    Fix: output not displaying

--- Question11/app.js
+++ Question11/app.js
@@ -1,13 +1,14 @@
 const input = document.querySelector("#password");
 const button = document.querySelector("#button");
-const output = document.querySelector(".output");
+const output = document.querySelector(".outputPara");
 
 button.addEventListener("click", ()=> {
+    // console.log("YOOYOYOYYO");
     const inputValue = input.value;
     if(inputValue.length >=  10) {
         output.innerText = "SUCCESS";
     }
     else {
-        output.innerText = "SUCCESS";
+        output.innerText = "FAIL!";
     }
 })
