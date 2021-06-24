30e519ddb418405de65c914c412e44d2a5ad7223
Adhish Bahl <adhishbahl0@gmail.com>
2021-06-24 21:10:32 +0530

    JS done

---
+++ Question14/app.js
@@ -0,0 +1,20 @@
+let peopleInfo = [
+    {
+        name: "Ram",
+        age: 25,
+        yuga: "Treta"
+    },
+    {
+        name: "Krishna",
+        age: 31,
+        yuga: "Dwapar"
+    }
+]
+
+
+if(peopleInfo[0].age > peopleInfo[1].age) {
+    console.log(peopleInfo[0].name);
+}
+else {
+    console.log(peopleInfo[1].name);
+}
