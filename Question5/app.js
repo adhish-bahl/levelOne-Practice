e6ad60505282d7e4b77d99ae32be713a11759098
Adhish Bahl <70909675+adhish-bahl@users.noreply.github.com>
2021-06-27 18:08:07 +0000

    Q5 done

--- Question5/app.js
+++ Question5/app.js
@@ -3,16 +3,25 @@ var studentName = [];
 var unitMarks = [];
 var prefinalMarks = [];
 var finalMarks = [];
-let sum = 0;
+var sum = [];
 
 for(let i = 0; i < 5; i++) {
   studentName[i] = readlineSync.question("Name: ");
   unitMarks[i] = readlineSync.question("Unit Test Marks: ");
   prefinalMarks[i] = readlineSync.question("Pre-final Marks: ");
   finalMarks[i] = readlineSync.question("Final Marks: ");
-  sum = sum + unitMarks[i] + prefinalMarks[i] + finalMarks[i];
+  sum[i] = unitMarks[i] + prefinalMarks[i] + finalMarks[i];
 }
 
-console.log(studentName.length);
+var max = 0;
+for(var i = 0; i < 5; i++) {
+  if(sum[max] < sum[i]) {
+    max = i;
+  }
+}
+
+console.log(studentName[max], " has the heighest marks!");
+
+// console.log(studentName.length);
 
 // console.log("yooo");
