4bae0f89c2b82f572598597cb9880e7990709eb2
Adhish Bahl <70909675+adhish-bahl@users.noreply.github.com>
2021-06-28 08:54:07 +0000

    Program done

--- Question17/script.js
+++ Question17/script.js
@@ -1,3 +1,13 @@
 const readLine-sync = require("readline-sync");
 let news = readlineSync.question("News: ");
 let sourse = readlineSync.question("Sourse: ");
+
+let fakeSourses = ["whatapp", "facebook", "telegram"];
+
+for(let i = 0; i < 3; i++) {
+  if(sourse.toLowerCase() == fakeSourses[i]) {
+    console.log("DOn't believe on news from FB or Whatsapp or Telegram.");
+  } else {
+    console.log("News is valid!!");
+  }
+}
