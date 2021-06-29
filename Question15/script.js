e14f7df33af5fc10bd1c8d2c440a6c92ee04e5ea
Adhish Bahl <70909675+adhish-bahl@users.noreply.github.com>
2021-06-29 14:48:36 +0000

    Input done

--- Question15/script.js
+++ Question15/script.js
@@ -1,3 +1,11 @@
 var readlineSync = require("readline-sync");
 let person1 = new Object;
 let person2 = new Object;
+
+person1.name = readlineSync.question("Name: ");
+person1.age = readlineSync.question("Age: ");
+person1.power = readlineSync.question("Power: ");
+
+person2.name = readlineSync.question("Name: ");
+person2.age = readlineSync.question("Age: ");
+person2.power = readlineSync.question("Power: ");
