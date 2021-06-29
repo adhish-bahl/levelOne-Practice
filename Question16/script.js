0b5ca4bf55d98c017e1b22e8ae2d450a3e1c7135
Adhish Bahl <70909675+adhish-bahl@users.noreply.github.com>
2021-06-29 15:11:34 +0000

    added comments

--- Question16/script.js
+++ Question16/script.js
@@ -1,17 +1,24 @@
+//importing readline-sync library
 var readlineSync = require("readline-sync");
 
+//declaring objects
 let person1 = new Object;
 let person2 = new Object;
 
+//input for person 1
 person1.name = readlineSync.question("Name: ");
 person1.age = readlineSync.question("Age: ");
 person1.power = readlineSync.question("Power: ");
 
+//input for person 2
 person2.name = readlineSync.question("Name: ");
 person2.age = readlineSync.question("Age: ");
 person2.power = readlineSync.question("Power: ");
 
+//calling function
+checkPower(person1, person2);
 
+//function for comparing 
 function checkPower(object1, object2) {
   console.log("ACCORDING TO POWER!!");
   if(object1.power > object2.power) {
