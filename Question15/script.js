05823e2662b6c8945e7fa17cbdecc3a48f9c4a4e
Adhish Bahl <70909675+adhish-bahl@users.noreply.github.com>
2021-06-29 14:51:24 +0000

    Question done

--- Question15/script.js
+++ Question15/script.js
@@ -9,3 +9,13 @@ person1.power = readlineSync.question("Power: ");
 person2.name = readlineSync.question("Name: ");
 person2.age = readlineSync.question("Age: ");
 person2.power = readlineSync.question("Power: ");
+
+if(person1.power > person2.power) {
+  console.log(person1.name);
+}
+else if(person1.power < person2.power) {
+  console.log(person2.name);
+}
+else {
+  console.log("Both have the same power!!");
+}
